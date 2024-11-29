2a6da34e43f8229e297d9bb690d2aaef49dcf034
CodexIngeniare <np.ingeniare@gmail.com>
2024-11-29 18:02:34 +0100

    feat(front/components): Implement viewer component for data fetched from API
    - EndpointDataViewer has baseURL prop which designates baseURL of API.
    - Takes input for specific endpoint from which data is to be fetched.
    - Displays fetched data in json format OR error message.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './styles/App.css';
+import EndpointDataViewer from './components/EndpointDataViewer';
 
 function App() {
   return (
     <div className="App">
       <h1>Forum Ingeniorum</h1>
+      <EndpointDataViewer baseURL="http://127.0.0.1:5000"/>
     </div>
   );
 }
