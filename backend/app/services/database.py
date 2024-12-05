3336635f9ec66ce4c935d029a6e2466233bb4364
CodexIngeniare <np.ingeniare@gmail.com>
2024-12-05 22:54:42 +0100

    refactor(backend/database): Refactored is_email_registered functionc
    + Added to also check for pending user with seeked email.
    - Changed return values, from yes/no to True/False.
    
    This change was needed because if email is not also checked in pending users table same email couldve been registered multiple times.

--- backend/app/services/database.py
+++ backend/app/services/database.py
@@ -73,16 +73,22 @@ def log_user_login(email):
 
 def is_email_registered(email):
     """
-    Proverava da li je email već u upotrebi u tabeli registrovanih korisnika.
+    Proverava da li je email već u upotrebi u tabeli registrovanih i neodobrenih korisnika.
     :param email: Email adresa koja se proverava.
-    :return: "yes" ako je email već u upotrebi, "no" u suprotnom.
+
+    :return: True ako je email već u upotrebi, False u suprotnom.
     """
     try:
-        existing_user = RegisteredUser.query.filter_by(email=email).first()
-        return "yes" if existing_user else "no"
+        reg_user_with_email = RegisteredUser.query.filter_by(email=email).first()
+        if reg_user_with_email is None:
+            pen_user_with_email = PendingUser.query.filter_by(email=email).first()
+            if pen_user_with_email is None:
+                return False
+        
+        return True
     except SQLAlchemyError as e:
         print(f"Greška: {str(e)}")
-        return "no"
+        return True
     
 
 # Funkcija za registraciju novog korisnika
@@ -92,7 +98,7 @@ def register_new_user(email, form_data):
     """
     try:
         # Provera da li je email već u upotrebi
-        if is_email_registered(email) == "yes":
+        if is_email_registered(email) == True:
             return None  # Email već postoji, korisnik se ne registruje
 
         # Kreiranje novog korisnika i dodavanje u bazu
